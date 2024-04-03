cfa994ebee0e3f1b561fbc28cfa6e88e989c2abc
abinjos307@gmail.com <abinjos307@gmail.com>
2024-04-04 00:08:41 +0530

    Working on the navbar still not working. Problem  faced on the working of hamburger.Toggling not working. Still need some modification on the styling.

---
+++ app.js
@@ -0,0 +1,5 @@
+let toggleBtn = document.querySelector("toggle_btn");
+let dropMenu = document.querySelector("drop-menu");
+toggleBtn.addEventListener("click",()=>{
+    dropMenu.classList.add("open")
+})
