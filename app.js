868a69529790732247eab46acae263d284d54865
abinjos307@gmail.com <abinjos307@gmail.com>
2024-05-05 19:20:59 +0530

    Advanced cards section completed

--- app.js
+++ app.js
@@ -2,17 +2,6 @@
 let x
 const shortBtn = document.querySelector(".btn-shorten");
 shortBtn.addEventListener("click", async() => {
-    // for creating the link block when tiny url is been generted
-
-//   const existingDiv = document.querySelector('.link-block');
-//   existingDiv.style.opacity = "1";
-//   const newDiv = document.createElement('div');
-//   newDiv.textContent = "This is a new div element";
-//   const classListToAdd = ['link-list', 'col-lg-8', 'col-sm-10', 'mx-auto', 'py-3'];
-//   for (const className of classListToAdd) {
-//     newDiv.classList.add(className);
-//   }
-//   existingDiv.appendChild(newDiv);
 const linkField = document.querySelector(".link-field")
 
 
